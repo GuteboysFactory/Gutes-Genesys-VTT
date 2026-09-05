d12bb875b9e29494ffb97f3faa31e1e172588898
GuteboysFactory <per@guteboy.eu>
2026-09-05 20:56:32 +0200

    Bump Genesys VTT to 0.0.1722

--- dist/module/constants.js
+++ dist/module/constants.js
@@ -1,5 +1,5 @@
 export const SYSTEM_ID = "genesys-vtt";
-export const SYSTEM_VERSION = "0.0.1721";
+export const SYSTEM_VERSION = "0.0.1722";
 export const MIN_FOUNDRY_VERSION = "13.351";
 export const TARGET_FOUNDRY_GENERATIONS = Object.freeze([13, 14]);
 //# sourceMappingURL=constants.js.map
