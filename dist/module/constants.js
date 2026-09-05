8ba74031c5938ea560aa5206d341180ceb852e8d
GuteboysFactory <per@guteboy.eu>
2026-09-05 12:22:14 +0200

    Fix runtime version ordering for 0.0.170 milestone

--- dist/module/constants.js
+++ dist/module/constants.js
@@ -1,5 +1,5 @@
 export const SYSTEM_ID = "genesys-vtt";
-export const SYSTEM_VERSION = "0.0.170";
+export const SYSTEM_VERSION = "0.0.1700";
 export const MIN_FOUNDRY_VERSION = "13.351";
 export const TARGET_FOUNDRY_GENERATIONS = Object.freeze([13, 14]);
 //# sourceMappingURL=constants.js.map
